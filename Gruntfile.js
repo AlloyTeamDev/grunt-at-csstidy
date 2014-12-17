34ef6dac18e265145d6b4be6894e4fd23dac100b
lightningtgc <lightningtgc@gmail.com>
2014-12-17 19:29:07 +0800

    Add test for CI

---
+++ Gruntfile.js
@@ -0,0 +1,29 @@
+﻿
+'use strict';
+
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    csstidy: {
+        dynamic_mappings: {
+            expand: true, //  Enable dynamic expansion
+            cwd: 'test/css/', //Src matches are relative to this path. 
+            src: ['*.css', '!*.special.css'], // Actual pattern(s) to match.
+            dest: 'test/css/', //Destination path prefix.
+            ext: '.resorted.css', //Dest filepaths will have this extension
+            extDot: 'first'   // Extensions in filenames begin after the first dot
+        }
+    }   
+
+  });
+  
+  // Actually load this plugin's task(s).
+  grunt.loadTasks('tasks');
+  
+  grunt.registerTask('test', ['csstidy']);
+  // By default
+  grunt.registerTask('default', ['test']);
+  
+};
+
