baf8d64a86c885c74e0ce586d169a75f8335c530
lightningtgc <lightningtgc@gmail.com>
2014-12-17 16:42:20 +0800

    abstraction, update version and description

--- tasks/csstidy.js
+++ tasks/csstidy.js
@@ -20,23 +20,98 @@ module.exports = function (grunt) {
     grunt.registerMultiTask('csstidy', 'Sorting CSS flies,and make it tidy.', function () {
 
         var config = grunt.task.current.options().config || path.join(__dirname, '/config/config.json');
-        grunt.log.ok('Start soring css...');
-        if (config && grunt.file.exists(config)) {
-            config = grunt.file.readJSON(config);
-        } else {
-            grunt.log.ok('Using csscomb config file...');
-            config = csscomb.getConfig('csscomb');
-        }
 
-        // Configure csscomb:
-        comb.configure(config);
+        // Handle special case
+        function preHandleSrc (cssSrc) {
 
-        this.files.forEach(function (file) {
-            var dest = file.dest || '';
+            /* 
+             * fix like: .a{ filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#e6529dda', endColorstr='#e6529dda', GradientType=0)\9;}
+             * which has two semicolon( : ) will cause parse error.
+             */
+            cssSrc = cssSrc.replace(/progid(\s)?:(\s)?/g, '#tidy1#');
 
-            var srcs = file.src.filter(function (filepath) {
+            /*
+             * fix absolute url path like: 
+             * .a { background: url("http://www.qq.com/one.png");} 
+             *
+             */
 
-                // Warn on and remove invalid source files (if nonull was set).
+            cssSrc = cssSrc.replace(/:\/\//g, '#tidy2#');
+
+            /*
+             * fix base64 url like: 
+             * .a { background: url("data:image/png;base64,iVBOMVEX///////////////+g0jAqu8zdII=");} 
+             *
+             */
+            cssSrc = cssSrc.replace(/(?:data)(.*)(?=\))/g, function(match){
+                match = match.replace(/:/g, '#tidy3#');
+                match = match.replace(/;/g, '#tidy4#');
+                match = match.replace(/\//g, '#tidy5#');
+                return match;
+            });
+
+            /*
+             * fix multiple line comment include single comment //
+             * eg: / * sth // another * /
+             */
+            cssSrc = cssSrc.replace(/\/\*[\s\S]+?\*\//g, function(match){
+                // handle single comment //
+                match = match.replace(/\/\//g, '#tidy6#');
+                return  match;
+            });
+
+            /*
+             * fix single comment like:  // something 
+             * It can't works in IE, and will cause parse error
+             *
+             */
+            cssSrc = cssSrc.replace(/(^|[^:|'|"|\(])\/\/.+?(?=\n|\r|$)/g, function(match){
+                var targetMatch;
+                //handle first line
+                if (match.charAt(0) !== '/' ) {
+                    // remove first string and / and \ 
+                    targetMatch = match.substr(1).replace(/\\|\//g, '');;
+                    return match.charAt(0) + '/*' + targetMatch + '*/';
+                } else {
+                    targetMatch = match.replace(/\\|\//g, '');
+                    return '/*' + targetMatch + '*/';
+                }
+            });
+
+            return cssSrc;
+    }
+
+    // after handle src, recover special string
+    function afterHandleSrc (content) {
+
+        content = content.replace(/#tidy1#/g, 'progid:');
+        content = content.replace(/#tidy2#/g, '://');
+        content = content.replace(/#tidy3#/g, ':');
+        content = content.replace(/#tidy4#/g, ';');
+        content = content.replace(/#tidy5#/g, '/');
+        content = content.replace(/#tidy6#/g, '//');
+
+        return content;
+    }
+
+
+    grunt.log.ok('Start soring css...');
+    if (config && grunt.file.exists(config)) {
+        config = grunt.file.readJSON(config);
+    } else {
+        grunt.log.ok('Using csscomb config file...');
+        config = csscomb.getConfig('csscomb');
+    }
+
+    // Configure csscomb:
+    comb.configure(config);
+
+    this.files.forEach(function (file) {
+        var dest = file.dest || '';
+
+        var srcs = file.src.filter(function (filepath) {
+
+            // Warn on and remove invalid source files (if nonull was set).
                 if (!grunt.file.exists(filepath)) {
                     grunt.log.warn('Source file "' + filepath + '" not found.');
                     return false;
@@ -56,69 +131,11 @@ module.exports = function (grunt) {
                     var content = '';
                     
                     try{
-                        /* 
-                         * fix like: .a{ filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#e6529dda', endColorstr='#e6529dda', GradientType=0)\9;}
-                         * which has two semicolon( : ) will cause parse error.
-                         */
-                        cssSrc = cssSrc.replace(/progid(\s)?:(\s)?/g, '#ihack#');
 
-                        /*
-                         * fix absolute url path like: 
-                         * .a { background: url("http://www.qq.com/one.png");} 
-                         *
-                         */
-
-                        cssSrc = cssSrc.replace(/:\/\//g, '#iihack#');
-
-                         /*
-                         * fix base64 url like: 
-                         * .a { background: url("data:image/png;base64,iVBOMVEX///////////////+g0jAqu8zdII=");} 
-                         *
-                         */
-                        cssSrc = cssSrc.replace(/(?:data)(.*)(?=\))/g, function(match){
-                            match = match.replace(/:/g, '#iiihack#');
-                            match = match.replace(/;/g, '#iiiihack#');
-                            match = match.replace(/\//g, '#iiiiihack#');
-                            return match;
-                        });
-                        
-                         /*
-                         * fix multiple line comment include single comment //
-                         * eg: / * sth // another * /
-                         */
-                        cssSrc = cssSrc.replace(/\/\*[\s\S]+?\*\//g, function(match){
-                            grunt.log.ok(match);
-                            /* match.replace('/*', ''); */
-                            match = match.replace(/\/\//g, '#iiiiiihack#');
-                            return  match;
-                        });
-
-                        /*
-                         * fix single comment like:  // something 
-                         * It can't works in IE, and will cause parse error
-                         *
-                         */
-                        cssSrc = cssSrc.replace(/(^|[^:|'|"|\(])\/\/.+?(?=\n|\r|$)/g, function(match){
-                            var targetMatch;
-                            //handle first line
-                            if (match.charAt(0) !== '/' ) {
-                                // remove first string and / and \ 
-                                targetMatch = match.substr(1).replace(/\\|\//g, '');;
-                                return match.charAt(0) + '/*' + targetMatch + '*/';
-                            } else {
-                                targetMatch = match.replace(/\\|\//g, '');
-                                return '/*' + targetMatch + '*/';
-                            }
-                           
-                        });
-
+                        cssSrc  = preHandleSrc(cssSrc);
                         content = comb.processString(cssSrc, { syntax: syntax });
-                        content = content.replace(/#ihack#/g, 'progid:');
-                        content = content.replace(/#iihack#/g, '://');
-                        content = content.replace(/#iiihack#/g, ':');
-                        content = content.replace(/#iiiihack#/g, ';');
-                        content = content.replace(/#iiiiihack#/g, '/');
-                        content = content.replace(/#iiiiiihack#/g, '//');
+                        content = afterHandleSrc(content);
+
                     }catch(e){
                         grunt.log.fail('Some error in : '+ src + '\r' + e);
                         return;
